6f109f86612c53b0c00077cbaaad79f90189ffdc
EmircanTK <45520522+EmircanTK@users.noreply.github.com>
2018-12-11 21:54:25 +0300

    Python Hesap Makinesi

---
+++ calculator.py
@@ -0,0 +1,29 @@
+sayi1 = float(input("1. Sayı =>"))
+islem = input("İşlemi Giriniz (+,-,*,/) => ")
+sayi2 = float(input("2. Sayı =>"))
+
+if islem == "+":
+    sonuc = sayi1 + sayi2
+    print(sonuc)
+    
+elif islem == "-":
+    sonuc = sayi1 - sayi2
+    print(sonuc)
+    
+elif islem == "*":
+    sonuc = sayi1 * sayi2
+    print(sonuc)
+    
+elif islem == "/":
+    sonuc = sayi1 / sayi2
+    print(sonuc)
+    
+else:
+    print("Hatalı Giriş Yaptınız..!")
+
+
+
+
+
+    
+
